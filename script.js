cf25fa7be51a4b21562a7aae4b12c4b6f525c8d2
PASTEUR666 <gen.histoires.flippantes@gmail.com>
2025-04-16 09:23:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById('generateBtn').addEventListener('click', async () => {
     try {
-        const response = await fetch('http://localhost:3000/generate', {
+        const response = await fetch('http://localhost:3001/generate', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
